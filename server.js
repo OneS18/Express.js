1c3f29e57c11653f96a14e4cad80f0a3d87906dd
OneS18 <diablo201888@gmail.com>
2022-03-06 12:39:58 +0200

    finish with express tutorial

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require("express");
-const router = require("./routes/users");
+// const router = require("./routes/users");
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set("view engine", "ejs");
-
-app.get("/", logger, (req, res) => {
-  console.log("here");
-  res.render("index", { text: "World" });
-});
-
-app.get("/users", (req, res) => {
-  res.send("User List");
-});
-
-app.get("/users/new", (req, res) => {
-  res.send("Users New Form");
-});
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
-
-function logger(req, res, next) {
-  console.log(req.originalUrl);
-  next();
-}
 
 app.listen(7000);
